488527dc1ddbce138ff1ccef80146c60f9d0ea2b
Amber-Pittman <amber@amberpittman.com>
2020-05-13 18:20:50 -0400

    Created routers

---
+++ auth/auth-router.js
@@ -0,0 +1,85 @@
+const express = require("express")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const Users = require("../users/users-model")
+const restrict = require("../middleware/restrict")
+
+const router = express.Router()
+
+router.post("/register", async (req, res, next) => {
+	try {
+		const { username } = req.body
+		const user = await Users.findBy({ username }).first()
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		res.status(201).json(await Users.add(req.body))
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.post("/login", async (req, res, next) => {
+	const authError = {
+		message: "Invalid Credentials",
+	}
+
+	try {
+		const user = await Users.findBy({ username: req.body.username }).first()
+		if (!user) {
+			return res.status(401).json(authError)
+		}
+
+
+		// since bcrypt hashes generate different results due to the salting,
+		// we rely on the magic internals to compare hashes rather than doing it
+		// manually with "!=="
+		const passwordValid = await bcrypt.compare(req.body.password, user.password)
+		if (!passwordValid) {
+			return res.status(401).json(authError)
+		}
+
+		// creates a new session for the user and saves it in memory.
+		// it's this easy since we're using `express-session`
+		//req.session.user = user
+
+		// GENERATE A NEW TOKEN
+		const tokenPayload = {
+			userId: user.id,
+			userRole: "admin", // this would normally come from the database
+		}
+
+		//const token = jwt.sign(tokenPayload, "keep it secret, keep it safe") // payload and secret string
+
+		res.cookie("token", jwt.sign(tokenPayload, process.env.JWT_SECRET))
+
+		res.json({
+			message: `Welcome ${user.username}!`,
+			//token: token, <<<<< original version
+			//token: jwt.sign(tokenPayload, process.env.JWT_SECRET) //moved to cookie
+		})
+	} catch(err) {
+		next(err)
+	}
+})
+
+// router.get("/logout", restrict(), (req, res, next) => {
+// 	// this will delete the session in the database and try to expire the cookie,
+// 	// though it's ultimately up to the client if they delete the cookie or not.
+// 	// but it becomes useless to them once the session is deleted server-side.
+// 	req.session.destroy((err) => {
+// 		if (err) {
+// 			next(err)
+// 		} else {
+// 			res.json({
+// 				message: "Logged out",
+// 			})
+// 		}
+// 	})
+// })
+
+module.exports = router
